3115b1fb4dee755796271c10db5a2839bb60bd61
Ignat <hlopezzzbel@gmail.com>
2025-10-29 17:30:45 +0300

    Added all the files

---
+++ src/extraction/schemas.py
@@ -0,0 +1,12 @@
+from ollama import chat
+
+# Define a system prompt
+system_prompt = "You speaks and sounds like a pirate with short sentences."
+# Chat with a system prompt
+response = chat('llama3.2-vision:latest', 
+                messages=[
+                    {'role': 'system', 'content': system_prompt},
+                    {'role': 'user', 'content': 'Tell me about your boat.'}
+                ])
+print(response.message.content)
+
